f2288373d4b66993ff4e4a9d93a66923ce537468
diasmxm <stronghold996@mail.ru>
2021-10-12 21:58:55 +0300

    game

---
+++ js/index.js
@@ -0,0 +1,45 @@
+// функция, которая возращает случайное число от аргумента х до аргумента у
+// Math.random()
+// Math.round() - округление, математически честное
+// Math.floor() - округление вниз
+// Math.ceil() - округление вверх
+const materials = ['камень', 'ножницы', 'бумага']
+
+function createElement(tag, content, className){
+    let el = document.createElement(tag);
+    el.textContent = content;
+    el.classList.add(className)
+    document.body.appendChild(el)
+    el.onclick = () =>{
+        let choose = el.textContent;
+        let choose_comp = materials[getRandomNumber(2)]
+        if (choose === choose_comp) {
+            alert("ничья")
+        }else if(choose === 'бумага' && choose_comp === 'камень'){
+            alert("вы выиграли")
+        }else if(choose === 'ножницы' && choose_comp === 'камень'){
+            alert("вы проиграли")
+        }else if(choose === 'бумага' && choose_comp === 'ножницы'){
+            alert("вы проиграли")
+        }else if(choose === 'камень' && choose_comp === 'ножницы'){
+            alert("вы выиграли")
+        }else if(choose === 'камень' && choose_comp === 'бумага'){
+            alert("вы проиграли")
+        }else if(choose === 'ножницы' && choose_comp === 'бумага'){
+            alert("вы выиграли")
+        }
+    }
+}
+
+materials.map((item)=>{
+    createElement('button', item, "btn")
+})
+
+function getRandomNumber(y){
+    return Math.round(Math.random()*y)
+}
+
+
+
+console.log(getRandomNumber(3));
+
---
+++ js/primitiv.js
@@ -0,0 +1,13 @@
+let x = 14;
+x = 15;
+console.log(x)
+
+let obj = {
+    x: 15,
+    y: 17
+}
+let obj2 = obj;
+obj2.x = 24;
+obj2.y = 100
+
+console.log(obj.x); //что будет на 11 строке?
